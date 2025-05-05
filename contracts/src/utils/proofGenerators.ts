5f2ecce2b9696f75919a91d687710f6431d06c21
Kadircan Bozkurt <kadircanbozkurt1905@gmail.com>
2025-05-05 11:13:04 +0300

    feat: compress isOdd field of pubkeys in proofGenerators class

--- contracts/src/utils/proofGenerators.ts
+++ contracts/src/utils/proofGenerators.ts
@@ -4,16 +4,18 @@ import { AGGREGATE_THRESHOLD } from './constants';
 export { ProofGenerators };
 
 class ProofGenerators extends Struct({
-  list: Provable.Array(Field, AGGREGATE_THRESHOLD),
+  // AGGREGATE_THRESHOLD -> PublicKey x Field
+  // 1 -> PublicKey isOdd Field concatenated
+  list: Provable.Array(Field, AGGREGATE_THRESHOLD + 1),
 }) {
   static empty() {
-    const list = Array<Field>(AGGREGATE_THRESHOLD).fill(Field.from(0));
+    const list = Array<Field>(AGGREGATE_THRESHOLD + 1).fill(Field.from(0));
     return new ProofGenerators({ list });
   }
 
   isEmpty() {
     let empty = Bool(true);
-    for (let i = 0; i < AGGREGATE_THRESHOLD; i++) {
+    for (let i = 0; i < AGGREGATE_THRESHOLD + 1; i++) {
       empty = empty.and(this.list[i].equals(Field(0)));
     }
     return empty;
@@ -25,39 +27,60 @@ class ProofGenerators extends Struct({
         `Array length must be ${AGGREGATE_THRESHOLD}, but got ${arr.length}`
       );
     }
-    const list = arr.map((pubKey) => {
-      return Poseidon.hash(pubKey.toFields());
-    });
+    const list = this.empty().list;
+    for (let i = 0; i < AGGREGATE_THRESHOLD; i++) {
+      list[i] = arr[i].x;
+      list[AGGREGATE_THRESHOLD] = list[AGGREGATE_THRESHOLD].add(
+        Number(arr[i].isOdd.toField().toBigInt()) * 2 ** i
+      );
+    }
     return new ProofGenerators({ list });
   }
 
-  static fromFieldArray(arr: Array<Field>) {
-    if (arr.length !== AGGREGATE_THRESHOLD) {
-      throw new Error(
-        `Array length must be ${AGGREGATE_THRESHOLD}, but got ${arr.length}`
-      );
-    }
-    return new ProofGenerators({ list: arr });
-  }
-
-  insertAt(index: Field, value: Field) {
+  insertAt(index: Field, publicKey: PublicKey) {
+    let power = Field(1);
     for (let i = 0; i < AGGREGATE_THRESHOLD; i++) {
-      this.list[i] = Provable.if(index.equals(Field(i)), value, this.list[i]);
+      this.list[i] = Provable.if(
+        index.equals(Field(i)),
+        publicKey.x,
+        this.list[i]
+      );
+      power = Provable.if(Field(i).lessThan(index), power.add(power), power);
     }
+    // Todo first reset
+    this.list[AGGREGATE_THRESHOLD] = this.list[AGGREGATE_THRESHOLD].add(
+      power.mul(publicKey.isOdd.toField())
+    );
     return this;
   }
 
-  getAt(index: Field) {
-    let value = Field(0);
+  getXAt(index: Field) {
+    let x = Field(0);
     for (let i = 0; i < AGGREGATE_THRESHOLD; i++) {
-      value = Provable.if(index.equals(Field(i)), this.list[i], value);
+      x = Provable.if(index.equals(Field(i)), this.list[i], x);
     }
-    return value;
+    return x;
+  }
+
+  getIsOddAt(index: Field) {
+    let isOdd = Bool(false);
+    let isOddFields =
+      this.list[AGGREGATE_THRESHOLD].toBits(AGGREGATE_THRESHOLD);
+    for (let i = 0; i < AGGREGATE_THRESHOLD; i++) {
+      isOdd = Provable.if(index.equals(Field(i)), isOddFields[i], isOdd);
+    }
+    return isOdd;
+  }
+
+  getPublicKeyAt(index: Field) {
+    const x = this.getXAt(index);
+    const isOdd = this.getIsOddAt(index);
+    return PublicKey.fromValue({ x, isOdd });
   }
 
   assertEquals(other: ProofGenerators) {
     let equal = Bool(true);
-    for (let i = 0; i < AGGREGATE_THRESHOLD; i++) {
+    for (let i = 0; i < AGGREGATE_THRESHOLD + 1; i++) {
       equal = equal.and(this.list[i].equals(other.list[i]));
     }
     equal.assertTrue('Proof generators list mismatch');
@@ -73,17 +96,23 @@ class ProofGenerators extends Struct({
 
   appendList(length: Field, other: ProofGenerators) {
     let newList = ProofGenerators.empty();
+    let power = Field(1);
     for (let i = 0; i < AGGREGATE_THRESHOLD; i++) {
       newList.list[i] = Provable.if(
         Field(i).lessThan(length),
         this.list[i],
         Provable.if(
           Field(i).greaterThanOrEqual(length),
-          other.getAt(Field(i).sub(length)),
+          other.getXAt(Field(i).sub(length)),
           Field(0)
         )
       );
+      power = Provable.if(Field(i).lessThan(length), power.add(power), power);
     }
+
+    newList.list[AGGREGATE_THRESHOLD] = this.list[AGGREGATE_THRESHOLD].add(
+      power.mul(other.list[AGGREGATE_THRESHOLD])
+    );
     return newList;
   }
 }
