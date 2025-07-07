226dbb4b567ea3e83a03a644f9e08fc0b7e734c7
Kadircan Bozkurt <kadircanbozkurt1905@gmail.com>
2025-07-07 20:47:01 +0300

    feat: optimize compiling

--- prover/src/utils.ts
+++ prover/src/utils.ts
@@ -8,24 +8,25 @@ import logger from "./logger.js";
 
 export { compileContracts, prettierAddress };
 
-async function compileContracts() {
+async function compileContracts(mode: "settlement" | "reducer") {
     try {
         logger.info("Compiling contracts...");
         let time = performance.now();
         await MultisigVerifierProgram.compile();
         console.log(`MultisigVerifierProgram compiled in ${performance.now() - time} ms`);
-        time = performance.now();
+        if (mode === "reducer") {
+            time = performance.now();
+            await ValidateReduceProgram.compile();
+            console.log(`ValidateReduceProgram compiled in ${performance.now() - time} ms`);
 
-        await ValidateReduceProgram.compile();
-        console.log(`ValidateReduceProgram compiled in ${performance.now() - time} ms`);
+            time = performance.now();
+            await ActionStackProgram.compile();
+            console.log(`ActionStackProgram compiled in ${performance.now() - time} ms`);
 
-        time = performance.now();
-        await ActionStackProgram.compile();
-        console.log(`ActionStackProgram compiled in ${performance.now() - time} ms`);
-
-        time = performance.now();
-        await SettlementContract.compile();
-        console.log(`SettlementContract compiled in ${performance.now() - time} ms`);
+            time = performance.now();
+            await SettlementContract.compile();
+            console.log(`SettlementContract compiled in ${performance.now() - time} ms`);
+        }
     } catch (err) {
         throw new Error(`Failed to compile contracts: ${err}`);
     }
