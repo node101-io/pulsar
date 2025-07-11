eb81ba3d81758c48da093e53304ce2111b49d79d
Kadircan Bozkurt <kadircanbozkurt1905@gmail.com>
2025-07-11 17:17:21 +0300

    feat: validator signer node skeleton

---
+++ signer-node/src/index.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import { PrivateKey, Signature } from "o1js";
+import { PulsarAction, TestUtils, ValidateReducePublicInput, validatorSet } from "pulsar-contracts";
+
+const minaPrivateKey = process.env.MINA_PRIVATE_KEY;
+if (!minaPrivateKey) {
+    throw new Error("Mina private key is not specified in environment variables");
+}
+const privateKey = PrivateKey.fromBase58(minaPrivateKey);
+const publicKey = privateKey.toPublicKey();
+
+const port = Number(process.env.PORT ?? 6000);
+
+const app = express();
+app.use(express.json());
+
+app.post("/sign", (req, res) => {
+    const { blockHeight, actions } = req.body;
+    if (!blockHeight || !actions)
+        return res.status(400).json({ error: "body must contain { blockHeight, actions }" });
+
+    try {
+        const typedActions = actions.map((action: { actions: string[][]; hash: string }) => {
+            return {
+                action: PulsarAction.fromRawAction(action.actions[0]),
+                hash: BigInt(action.hash),
+            };
+        });
+        const { publicInput } = TestUtils.CalculateFromMockActions(
+            ValidateReducePublicInput.default, // Todo: use correct public input
+            typedActions
+        );
+        const signature = Signature.create(privateKey, publicInput.hash().toFields());
+
+        res.json({
+            blockHeight,
+            validatorPubKey: publicKey.toBase58(),
+            signature: JSON.stringify(signature.toJSON()),
+        });
+    } catch (error) {
+        console.error("Error signing:", error);
+        res.status(500).json({ error: "Failed to sign the request" });
+    }
+});
+
+app.listen(port, () => console.log(`Signer up on http://localhost:${port}/sign`));
