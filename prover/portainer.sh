ccc1d6edf7e330aaa1a3e0ec5240f4cf177949db
Kadircan Bozkurt <kadircanbozkurt1905@gmail.com>
2025-07-17 17:21:21 +0300

    bash

---
+++ prover/portainer.sh
@@ -0,0 +1,6 @@
+docker volume create portainer_data
+docker run -d -p 9443:9443 -p 9000:9000 --name=portainer \
+  --restart=always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v portainer_data:/data \
+  portainer/portainer-ce
