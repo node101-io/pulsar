943674e6439607128717840c5f973bb6e7decd85
umut.eth <u.satir2021@gtu.edu.tr>
2026-02-02 19:25:29 +0300

    block prover model worker side

--- prover_v2/src/modules/processors/block-prover/worker.ts
+++ prover_v2/src/modules/processors/block-prover/worker.ts
@@ -1,4 +1,132 @@
-import { WithId } from "mongodb";
-import { BlockDoc } from "../../db/interfaces";
+import { ObjectId, WithId } from "mongodb";
+import { BlockDoc, ProofEpochDoc } from "../../db/interfaces";
+import { ProofStatus } from "../../db/types";
+import { DB } from "../../db";
+import { TIMEOUT_TIME_MS } from "../../utils/constants";
 
-export async function worker(task: WithId<BlockDoc>) {}
+export async function worker(task: WithId<BlockDoc>) {
+    const db = new DB();
+    await db.initMongo();
+
+    const height = task.height;
+
+    // set block status to done immediately to prevent errors when worker crashes at the status step
+    await setBlockStatusDone(db, height);
+
+    // change proof epoch's specific proof slot to processing (preventing concurrency issues)
+    await registerProofEpoch(height);
+
+    const proofId = await createProof(db, task);
+
+    if (height % 16 == 0) {
+        await createProofEpoch(height, proofId);
+    } else {
+        setProofOnEpoch(db, height, proofId).then((epoch) => {
+            if (epoch) {
+                setBlockStatusDone(db, height).then(() => {
+                    // TODO: Add logger info
+                });
+            } else {
+                db.proofsCol.findOneAndDelete({ _id: proofId });
+
+                // TODO: Add logger warn
+            }
+        });
+    }
+}
+
+async function createProof(db: DB, block: BlockDoc) {
+    // TODO: create proof logic here
+
+    const proof = await db.storeProof("test-proof-data");
+
+    // TODO: Add logger info
+
+    return proof; // return proof id
+}
+
+async function createProofEpoch(height: number, proofId: ObjectId) {
+    const db = new DB();
+    await db.initMongo();
+
+    const epochHeight = height - (height % 16);
+
+    await db.proofEpochsCol.findOneAndUpdate(
+        { height: epochHeight },
+        {
+            $setOnInsert: {
+                height: epochHeight,
+                proofs: Array(31).fill(null),
+                status: Array(16).fill("waiting" as ProofStatus),
+                failCount: 0,
+                timeoutAt: new Date(Date.now() + TIMEOUT_TIME_MS),
+            },
+            $set: {
+                [`proofs.${height % 16}`]: proofId,
+            },
+        },
+        { upsert: true },
+    );
+
+    // TODO: Add logger info
+}
+
+/**
+ * Change proof epoch's specific proof slot to 'processing' (preventing concurrency issues)
+ **/
+async function registerProofEpoch(height: number) {
+    const db = new DB();
+    await db.initMongo();
+
+    await db.proofEpochsCol.updateOne(
+        {
+            height: height - (height % 16),
+            [`status.${height % 16}`]: "waiting",
+        },
+        {
+            $set: {
+                [`status.${height % 16}`]: "processing",
+            },
+        },
+    );
+}
+
+/**
+ * Set proof ID on epoch for specific height
+ * @param db Database instance
+ * @param height Block height
+ * @param proofId Proof ObjectId
+ * @return Updated ProofEpochDoc or null if not found
+ */
+async function setProofOnEpoch(
+    db: DB,
+    height: number,
+    proofId: ObjectId,
+): Promise<WithId<ProofEpochDoc> | null> {
+    return db.proofEpochsCol.findOneAndUpdate(
+        {
+            height: height - (height % 16),
+            [`status.${height % 16}`]: "processing",
+        },
+        {
+            $set: {
+                [`proofs.${height % 16}`]: proofId,
+                [`status.${height % 16}`]: "done" as ProofStatus,
+            },
+        },
+    );
+}
+
+async function setBlockStatusDone(db: DB, height: number) {
+    await db.blocksCol.findOneAndUpdate(
+        {
+            height: height,
+            status: { $ne: "done" },
+        },
+        {
+            $set: {
+                status: "done",
+            },
+        },
+    );
+}
