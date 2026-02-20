c840d713500ab99166befdad138ed87b0fa22306
umut.eth <u.satir2021@gtu.edu.tr>
2026-02-20 16:30:47 +0300

    monitor module implemented

---
+++ prover_v2/src/modules/monitor/index.ts
@@ -0,0 +1,1 @@
+export { startMonitor } from "./monitor.js";
--- prover_v2/src/modules/utils/constants.ts
+++ prover_v2/src/modules/utils/constants.ts
@@ -11,3 +11,7 @@ export const PROOF_EPOCH_SETTLEMENT_INDEX = PROOF_EPOCH_LEAF_COUNT * 2 - 2;
 export const POLL_INTERVAL_MS = 5_000;
 export const TENDERMINT_SERVICE_NAME = "cosmos.base.tendermint.v1beta1.Service";
 export const MINA_KEYS_SERVICE_NAME = "interchain_security.minakeys.Query";
+
+// Monitor constants
+export const MAX_FAIL_COUNT = 3;
+export const MONITOR_INTERVAL_MS = 30_000; // 30 seconds
