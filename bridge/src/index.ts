414b0cbc9a267161d15009ac95c2623d9cffbda5
umut.eth <u.satir2021@gtu.edu.tr>
2026-06-11 22:14:43 +0300

    index file function misuse fixed

--- bridge/src/index.ts
+++ bridge/src/index.ts
@@ -2,11 +2,9 @@ import "dotenv/config";
 import { initDb } from "./db/connection.js";
 import { runStartup } from "./startup.js";
 import { startMinaSync } from "./services/mina/sync.js";
-import { initMinaNetwork } from "./services/mina/client.js";
 import logger from "./common/logger.js";
 
 async function main() {
-    initMinaNetwork();
     await initDb();
     await runStartup();
 
